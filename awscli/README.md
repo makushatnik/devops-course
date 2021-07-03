b6ed4cbb1bdf100f33f768e8e8688e22e754787a
Evgeniy Ageev <eageev@sinergo.ru>
2021-07-03 19:49:37 +0300

    Task-07: Added steps 1-3

--- awscli/README.md
+++ awscli/README.md
@@ -1,7 +1,16 @@
 # The AWS CLI task solution.
 
 ## Preparing.
-You need to install the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-linux.html) on your system.
+1. Install the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-linux.html) on your system.
+2. Go to your AWS Console and create an Access Keys.
+Either **Access key ID** and **Secret access key** needed before the next step.
+3. Run command:
+`aws configure`
+and enter your keys.
+4. Install jq by typing:
+    sudo wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+    sudo chmod +x ./jq
+    sudo cp jq /usr/bin
 
 ## Running.
 Run a script by typing:
--- awscli/script.sh
+++ awscli/script.sh
@@ -1,6 +1,100 @@
 #!/bin/bash
-readonly SETTED="which awscli"
-if [[ SETTED ]]
-	echo "AWS CLI installed"
+
+ARGS=("$*")
+echo "ARGS = $ARGS"
+
+Intervals=('day' 'hour' 'minute'); 
+
+readonly MAX_COUNT=2000000
+
+readonly RED="\e[91m"
+readonly GREEN="\e[32m"
+readonly YELLOW="\e[33m"
+readonly ENDCOLOR="\e[0m"
+
+readonly CANT_EXECUTE_MSG="${RED}Can't execute the script${ENDCOLOR}"
+readonly EBS_LIST_COMMAND_START="aws ec2 describe-snapshots --owner self --output json | jq '.Snapshots[] |"
+readonly EBS_LIST_COMMAND_END="| [.Description, .VolumeSize, .StartTime, .SnapshotId, .Tags]'"
+# readonly DATE_CONDITION=".StartTime < $(date --date='-"$1 $2"' '+%Y-%m-%d %H:%M')"
+readonly DATE_CONDITION=".StartTime < $(date --date='-1 hour' '+%Y-%m-%d %H:%M')"
+echo "DATE_CONDITION = $DATE_CONDITION"
+readonly FILTER_CONDITION=".Tags[].Value == $3"
+
+show_usage() {
+  echo "USAGE: script.sh <COUNT> <INTERVAL> <FILTER>"
+  echo ""
+  echo "* COUNT: numeric greater or equal to 0, but less than $MAX_COUNT"
+  echo "* INTERVAL: day, hour or minute"
+  echo "  FILTER: enter a Tag Value to filter Snapshots by"
+  echo ""
+  echo "Interval & count are used to show all EBS blocks those are older than count of days (hours or minutes)."
+  echo "* - required parameters"
+}
+
+check_requirements() {
+# Check if aws and jq are installed.
+  if [[ -z "$(which aws)" ]]; then
+    echo -e $CANT_EXECUTE_MSG
+    echo -e "Package ${RED}awscli${ENDCOLOR} is required"
+    echo -e "Please, install it: https://docs.aws.amazon.com/cli/latest/userguide/install-linux.html"
+    exit 1
+  fi
+
+  if [[ -z "$(which jq)" ]]; then
+    echo -e $cant_execute_msg
+    echo -e "Package ${RED}jq${ENDCOLOR} is required"
+    exit 1
+  fi
+}
+
+check_arguments() {
+  count=$1
+  interval=$2
+  if [[ -n count ]]; then
+    echo -e $CANT_EXECUTE_MSG
+    echo "Count = $count"
+    show_usage
+    exit 1
+  fi
+  if [[ count -lt 0 ]]; then
+    echo -e $CANT_EXECUTE_MSG
+    echo "Count should be >= 0"
+    show_usage
+    exit 1
+  fi
+  if [[ count -gt $MAX_COUNT ]]; then
+    echo -e $CANT_EXECUTE_MSG
+    echo "Count should be <= $MAX_COUNT"
+    show_usage
+    exit 1
+  fi
+
+  if [[ -n interval ]]; then
+    echo -e $CANT_EXECUTE_MSG
+    echo "Interval = $interval"
+    show_usage
+    exit 1
+  fi
+  found=`echo ${Intervals[*]} | grep "$interval"`
+  if [[ "${found}" == "" ]]; then
+    echo "Interval ain't found"
+    echo -e $CANT_EXECUTE_MSG
+    show_usage
+    exit 1
+  fi
+}
+
+### Main section
+check_requirements
+check_arguments
+
+FILTER=$3
+EBS_LIST=""
+
+if [[ -n $FILTER ]]; then
+  EBS_LIST="$EBS_LIST_COMMAND_START select($DATE_CONDITION and $FILTER_CONDITION) $EBS_LIST_COMMAND_END"
+else
+  EBS_LIST="$EBS_LIST_COMMAND_START select($DATE_CONDITION) $EBS_LIST_COMMAND_END"
 fi
-echo "You need to install the AWS CLI first"
+echo "List of EBS Snapshots found:"
+echo "$EBS_LIST"
