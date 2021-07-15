f29aa6ed8fe7755175653eac20cb782573e95ddb
Evgeniy Ageev <eageev@sinergo.ru>
2021-07-16 02:31:42 +0300

    TASK-07: Part 2

--- awscli/awscli.py
+++ awscli/awscli.py
@@ -1,23 +1,29 @@
 # Script for working with AWS CLI
 import os
+import sys
 import click
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
 intervals=['day','hour','minute']
 
+# Constants
 EBS_LIST_COMMAND_START="aws ec2 describe-snapshots --owner self --output json | jq '.Snapshots[] |"
 EBS_LIST_COMMAND_END="| [.Description, .VolumeSize, .StartTime, .SnapshotId, .Tags]'"
-#DATE_CONDITION=".StartTime < date --date='-{count} {interval}' '+%Y-%m-%d %H:%M'"
+CREATE_BUCKET_COMMAND="aws s3 mb s3://"
+MAX_COUNT=2000000
+BUCKET="makushatnik-ebs"
 
 def show_usage():
-  print("USAGE: python3 awscli.py <ARGS> <FILTER>")
+  print("USAGE: python3 awscli.py <ARGS> <OPTIONS>")
   print("")
   print("ARGS:")
-  print("  count   Number of interval measures")
+  print("  count   Number of interval measures. Less than " + str(MAX_COUNT))
   print("  interval   Chosen interval measure")
-  print("FILTER:")
-  print("  --tag, -t   Tag Value for filtering result list")
+  print("OPTIONS:")
+  print("  --tag, -t    Tag Value for filtering result list")
+  print("  --save, -s   Flag for saving snapshot in S3 by its ID")
+  print("  --help       Help")
   print("")
 
 @click.command()
@@ -27,22 +33,61 @@ def show_usage():
     '--tag', '-t',
     help='Tag Value for filtering result list'
 )
-def main(count, interval, **kwargs):
+@click.option(
+    '--save', '-s',
+    help='Save snapshots into S3 bucket flag'
+)
+def main(count, interval, tag, save):
+  """
+  An AWS CLI tool for checking EBS Snapshots and saving them. Provide count and interval of snapshots' age.
+
+  count   Number of interval measures. Less than 2000000.
+
+  interval   Chosen interval measure. Possible values: day, hour, minute
+  """
+  check_count(count)
   if interval not in intervals:
     show_usage()
     return "Incorrect interval argument"
 
-#  date_str="--date='-{} {}' '+%Y-%m-%d %H:%M'".format(count, interval)
+  show_ebs_list(count, interval, tag)
+  if save:
+    save_into_s3()
+
+def check_count(count):
+  if int(count) >= MAX_COUNT:
+    show_usage()
+    sys.exit()
+
+def save_into_s3():
+# TODO: Should we check if the bucket exists?
+  os.system(CREATE_BUCKET_COMMAND + BUCKET)
+  snapshot_id=input("Enter an EBS Snapshot Id: ")
+  if snapshot_id:
+    print(snapshot_id)
+    # TODO: cp or mv to an S3
+
+def show_ebs_list(count, interval, tag):
   date_str="--date=-{} {}".format(count, interval)
-  print(date_str)
   p=Popen(["date", date_str, "+%Y-%m-%d %H:%M"], stdout=PIPE)
   date_tmp=p.communicate()[0]
   date_str=date_tmp.decode()
-  print(date_str)
+  # WA
+  date_str=date_str.replace("\r", "")
+  date_str=date_str.replace('\n', '')
+
   date_condition=".StartTime < \"{}\"".format(date_str)
-  print(date_condition)
-  print(f"{EBS_LIST_COMMAND_START} select({date_condition}) {EBS_LIST_COMMAND_END}")
-  os.system(EBS_LIST_COMMAND_START + " select(" + date_condition + ")" + EBS_LIST_COMMAND_END)
+  os.system(get_command_str(date_condition, tag))
+
+def get_command_str(date_condition, tag):
+  command_str=EBS_LIST_COMMAND_START + " select(" + date_condition
+  if tag:
+    command_str+=" and .Tags[].Value == \"{}\") ".format(tag)
+  else:
+    command_str+=") "
+  command_str+=EBS_LIST_COMMAND_END
+  print(command_str)
+  return command_str
 
 if __name__ == "__main__":
   main()
